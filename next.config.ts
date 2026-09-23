7a7ea9f2531e8c21828e3a7ed3eb9f99c26afa74
Danielpvbrbr <dntechpvbr@gmail.com>
2026-09-23 00:29:30 -0300

    atualizando e corrigindo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['psiumeet.psiuclass.com.br'],
+  allowedDevOrigins: ['psiumeet.psiuclass.com.br', 'http://localhost:3000'],
+
+  async headers() {
+    return [
+      // Regra para liberar CORS nas rotas de API
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' }, 
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
+        ],
+      },
+      // Suas permissões de câmera e microfone
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(self), microphone=(self), display-capture=(self)',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
